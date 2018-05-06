2172352f88d4fc48efbe6b81d8094e4674210cb1
Emilio Forrer <dev.emilio.forrer@gmail.com>
2018-05-06 12:02:01 -0600

    Adding package information

---
+++ index.js
@@ -0,0 +1,16 @@
+ 'use strict'
+/**
+ * @Author: Emilio Forrer <emilioforrer>
+ * @Date:   2018-05-06T11:53:20-06:00
+ * @Email:  dev.emilio.forrer@gmail.com
+ * @Filename: index.js
+ * @Last modified by:   emilioforrer
+ * @Last modified time: 2018-05-06T12:01:21-06:00
+ * @License: For the full license information, please view the LICENSE
+ * @Copyright: (c) Emilio Forrer
+ */
+
+ module.exports = {
+   TranslatableProvider: require('./src/providers/TranslatableProvider'),
+   TranslatableTrait: require('./src/Models/Traits/TranslatableTrait')
+ }
